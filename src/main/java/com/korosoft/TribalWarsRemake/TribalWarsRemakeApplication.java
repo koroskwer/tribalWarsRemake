891a8bffe7dc34dd05ff5bc47d6048b846b7ca94
Koro <jakubbereda1@gmail.com>
2024-05-24 18:56:25 +0200

    Initial commit

---
+++ src/main/java/com/korosoft/TribalWarsRemake/TribalWarsRemakeApplication.java
@@ -0,0 +1,13 @@
+package com.korosoft.TribalWarsRemake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TribalWarsRemakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TribalWarsRemakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korosoft/TribalWarsRemake/TribalWarsRemakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korosoft.TribalWarsRemake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TribalWarsRemakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
