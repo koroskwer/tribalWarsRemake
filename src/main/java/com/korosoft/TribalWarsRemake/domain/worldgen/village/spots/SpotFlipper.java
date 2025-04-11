b9b5df548fbf36bf903782cc028bc4af9198756d
Koro <jakubbereda1@gmail.com>
2025-04-11 21:55:02 +0200

    Add faster spot generation algorithm implementation

---
+++ src/main/java/com/korosoft/TribalWarsRemake/domain/worldgen/village/spots/SpotFlipper.java
@@ -0,0 +1,32 @@
+package com.korosoft.TribalWarsRemake.domain.worldgen.village.spots;
+
+import com.korosoft.TribalWarsRemake.config.GameConstants;
+import com.korosoft.TribalWarsRemake.domain.worldgen.WorldGenDirection;
+
+public class SpotFlipper {
+
+    public Spot createTransformedSpot(int x, int y, WorldGenDirection direction) {
+        return switch (direction) {
+            case NORTH_EAST -> this.createNorthEastSpot(x, y);
+            case NORTH_WEST -> this.createNorthWestSpot(x, y);
+            case SOUTH_EAST -> this.createSouthEastSpot(x, y);
+            case SOUTH_WEST -> this.createSouthWestSpot(x, y);
+        };
+    }
+
+    private Spot createNorthEastSpot(int x, int y) {
+        return new Spot(x + GameConstants.HALF_MAP_SIZE, GameConstants.HALF_MAP_SIZE - y - 1, WorldGenDirection.NORTH_EAST);
+    }
+
+    private Spot createNorthWestSpot(int x, int y) {
+        return new Spot(GameConstants.HALF_MAP_SIZE - x - 1, GameConstants.HALF_MAP_SIZE - y - 1, WorldGenDirection.NORTH_WEST);
+    }
+
+    private Spot createSouthEastSpot(int x, int y) {
+        return new Spot(x + GameConstants.HALF_MAP_SIZE, GameConstants.HALF_MAP_SIZE + y, WorldGenDirection.SOUTH_EAST);
+    }
+
+    private Spot createSouthWestSpot(int x, int y) {
+        return new Spot(GameConstants.HALF_MAP_SIZE - x - 1, GameConstants.HALF_MAP_SIZE + y, WorldGenDirection.SOUTH_WEST);
+    }
+}
--- src/main/java/com/korosoft/TribalWarsRemake/domain/worldgen/village/spots/impl/FastSpotGeneratorImpl.java
+++ src/main/java/com/korosoft/TribalWarsRemake/domain/worldgen/village/spots/impl/FastSpotGeneratorImpl.java
@@ -5,7 +5,7 @@ import com.korosoft.TribalWarsRemake.domain.worldgen.WorldGenDirection;
 import com.korosoft.TribalWarsRemake.domain.worldgen.village.spots.Spot;
 import com.korosoft.TribalWarsRemake.domain.worldgen.village.spots.SpotFlipper;
 import com.korosoft.TribalWarsRemake.domain.worldgen.village.spots.SpotGenerator;
-import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -13,11 +13,12 @@ import java.util.List;
 
 
 /**
- * Checks the entire Quarter of a square when searching for valid points inside the donut
+ * Checks only the points above the donut and ones inside it, column by column. After algorithm finds the last point
+ * in column, it skips the rest of the column and goes to next one.
  */
 @Component
-@AllArgsConstructor
-public class SlowSpotGeneratorImpl implements SpotGenerator {
+@Primary
+public class FastSpotGeneratorImpl implements SpotGenerator {
 
     private final SpotFlipper flipper = new SpotFlipper();
 
@@ -26,9 +27,10 @@ public class SlowSpotGeneratorImpl implements SpotGenerator {
         int xStart = 0;
         int xEnd = generationLevel * GameConstants.MAP_GENERATION_STEP_SIZE + 20;
 
-
-        int yStart = 0;
-        int yEnd = generationLevel * GameConstants.MAP_GENERATION_STEP_SIZE + 20;
+        // First Y value from previous row
+        int yStart = generationLevel * GameConstants.MAP_GENERATION_STEP_SIZE + 20;
+        boolean isyStartSet = false;
+        int yEnd = 0;
 
         int radiusStart = 0;
         int radiusEnd = 20;
@@ -41,11 +43,18 @@ public class SlowSpotGeneratorImpl implements SpotGenerator {
         List<Spot> spots = new ArrayList<>();
 
         for (int x = xStart; x <= xEnd; x++) {
-            for (int y = yStart; y <= yEnd; y++) {
+            for (int y = yStart; y >= yEnd; y--) {
                 if (isPointInsideDonut(x, y, radiusStart, radiusEnd)) {
+                    if (!isyStartSet) {
+                        yStart = y;
+                        isyStartSet = true;
+                    }
                     spots.add(this.flipper.createTransformedSpot(x, y, direction));
+                } else if (isyStartSet) {
+                    break;
                 }
             }
+            isyStartSet = false;
         }
         return spots;
     }
